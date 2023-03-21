a29c001ed138fd36284979080f2ffc832fd8b37c
m4st3 <runtycybin@gmail.com>
2023-03-21 19:22:17 +0100

    initial commit

---
+++ src/main/java/com/demo/inditex/demoinditex/DemoinditexApplication.java
@@ -0,0 +1,13 @@
+package com.demo.inditex.demoinditex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoinditexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoinditexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/inditex/demoinditex/DemoinditexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.inditex.demoinditex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoinditexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
