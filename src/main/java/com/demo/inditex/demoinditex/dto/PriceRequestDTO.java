1c1987af799ad6a3201d0e73de220a90ee9ba7b9
m4st3 <runtycybin@gmail.com>
2023-03-23 00:05:40 +0100

    request dto refactored

--- src/main/java/com/demo/inditex/demoinditex/dto/PriceRequestDTO.java
+++ src/main/java/com/demo/inditex/demoinditex/dto/PriceRequestDTO.java
@@ -1,8 +1,15 @@
 package com.demo.inditex.demoinditex.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 import java.time.LocalDate;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PriceRequestDTO implements Serializable {
     private LocalDate fechaAplicacion;
     private Long productId;
