f0b48062d4e94e24faf2c5473e9768cad60b711d
m4st3 <runtycybin@gmail.com>
2023-03-22 23:57:22 +0100

    Made repository implementations transactional and repository

--- src/main/java/com/demo/inditex/demoinditex/repository/impl/ProductRepoImpl.java
+++ src/main/java/com/demo/inditex/demoinditex/repository/impl/ProductRepoImpl.java
@@ -1,4 +1,9 @@
 package com.demo.inditex.demoinditex.repository.impl;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional(readOnly = true)
 public class ProductRepoImpl {
 }
