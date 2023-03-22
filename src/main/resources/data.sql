a2fc20501b2ab480b129686c38a965d6c75ad454
m4st3 <runtycybin@gmail.com>
2023-03-22 09:33:36 +0100

    initial sql data created

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO producto (brandid) VALUES (35455);
+INSERT INTO precios (start_date, end_date, price_list, priority, precio, curr, id_producto)
+VALUES ('2020-06-14-00.00.00', '2020-12-31-23.59.59', 1, 0, 35.50, 'EUR', 35455);
