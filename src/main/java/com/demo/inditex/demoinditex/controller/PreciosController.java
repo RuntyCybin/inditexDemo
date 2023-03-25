c7ffca3db02ebf3265c76fe1d4620d6471e5ac8b
m4st3 <runtycybin@gmail.com>
2023-03-25 17:58:27 +0100

    PreciosController refactor

--- src/main/java/com/demo/inditex/demoinditex/controller/PreciosController.java
+++ src/main/java/com/demo/inditex/demoinditex/controller/PreciosController.java
@@ -9,8 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
