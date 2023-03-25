3e21ef54925387cb60415d1ccc0626735eb4f00c
m4st3 <runtycybin@gmail.com>
2023-03-25 17:57:09 +0100

    PriceController.java renamed to PreciosController

---
+++ src/main/java/com/demo/inditex/demoinditex/controller/PreciosController.java
@@ -0,0 +1,31 @@
+package com.demo.inditex.demoinditex.controller;
+
+import com.demo.inditex.demoinditex.dto.PriceRequestDTO;
+import com.demo.inditex.demoinditex.dto.PriceResponseDTO;
+import com.demo.inditex.demoinditex.service.PrecioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+public class PreciosController {
+
+    private PrecioService precioService;
+
+    @Autowired
+    public PreciosController(PrecioService service) {
+        this.precioService = service;
+    }
+
+    @GetMapping("/validatePrices")
+    public ResponseEntity<List<PriceResponseDTO>> getPrices(
+            @RequestBody PriceRequestDTO priceRequestDTO) {
+        return new ResponseEntity<>(precioService.validate(priceRequestDTO), HttpStatus.OK);
+    }
+}
