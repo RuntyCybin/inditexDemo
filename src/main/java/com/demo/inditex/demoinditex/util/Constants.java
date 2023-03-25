6e1546f5841e8aea21a78961f95a4b48261b7e10
m4st3 <runtycybin@gmail.com>
2023-03-25 18:34:15 +0100

    new constants added

--- src/main/java/com/demo/inditex/demoinditex/util/Constants.java
+++ src/main/java/com/demo/inditex/demoinditex/util/Constants.java
@@ -6,4 +6,5 @@ public class Constants {
     public final static String FECHA_APLICACION_ERROR = "Field fechaAplicacion cannot be null";
     public final static String PRODUCT_ID_ERROR = "Field productId cannot be null";
     public final static String BRAND_ID_ERROR = "Field brandId cannot be null";
+    public final static String PRECIO_NOT_FOUND_ERROR = "No se encontraron precios con estos parametros";
 }
