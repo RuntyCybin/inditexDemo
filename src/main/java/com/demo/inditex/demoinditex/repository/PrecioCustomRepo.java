8b2c1744590800725c3cf18212eae9434910495e
m4st3 <runtycybin@gmail.com>
2023-03-25 18:15:42 +0100

    Repositories update

--- src/main/java/com/demo/inditex/demoinditex/repository/PrecioCustomRepo.java
+++ src/main/java/com/demo/inditex/demoinditex/repository/PrecioCustomRepo.java
@@ -1,4 +1,10 @@
 package com.demo.inditex.demoinditex.repository;
 
+import com.demo.inditex.demoinditex.dto.PriceRequestDTO;
+import com.demo.inditex.demoinditex.entitie.Precios;
+
+import java.util.List;
+
 public interface PrecioCustomRepo {
+    public List<Precios> findPreciosPorRequest(PriceRequestDTO priceRequestDTO);
 }
--- src/main/java/com/demo/inditex/demoinditex/repository/impl/PrecioRepoImpl.java
+++ src/main/java/com/demo/inditex/demoinditex/repository/impl/PrecioRepoImpl.java
@@ -1,9 +1,37 @@
 package com.demo.inditex.demoinditex.repository.impl;
 
+import com.demo.inditex.demoinditex.dto.PriceRequestDTO;
+import com.demo.inditex.demoinditex.entitie.Precios;
+import com.demo.inditex.demoinditex.repository.PrecioCustomRepo;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 
 @Repository
 @Transactional(readOnly = true)
-public class PrecioRepoImpl {
+public class PrecioRepoImpl implements PrecioCustomRepo {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Override
+    public List<Precios> findPreciosPorRequest(PriceRequestDTO priceRequestDTO) {
+
+        List<Object> responseObj = entityManager.createQuery("FROM Precios pr " +
+                        "WHERE (:fechaApp BETWEEN pr.start_date AND pr.end_date) " +
+                        "AND pr.brand_id = :idCad " +
+                        "AND pr.producto.id = :idProd " +
+                        "ORDER BY pr.priority")
+                .setParameter("fechaApp", priceRequestDTO.getFechaAplicacion())
+                .setParameter("idProd", priceRequestDTO.getProductId())
+                .setParameter("idCad", priceRequestDTO.getBrandId())
+                .getResultList();
+
+        return responseObj.stream()
+                .filter(Precios.class::isInstance)
+                .map(Precios.class::cast)
+                .toList();
+    }
 }
