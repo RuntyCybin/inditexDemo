44788df7e71c3af315612aa066144feff3b50d0f
m4st3 <runtycybin@gmail.com>
2023-03-25 17:55:22 +0100

    Modified the real cases for the initial data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO producto (id) VALUES (35455);
-INSERT INTO precios (start_date, end_date, brand_id, price_list, priority, precio, curr, id_producto) VALUES ('2020-06-14-00.00.00', '2020-12-31-23.59.59', 1, 1, 0, 35.50, 'EUR', 35455);
-INSERT INTO precios (start_date, end_date, brand_id, price_list, priority, precio, curr, id_producto) VALUES ('2020-06-14-15.00.00', '2020-06-14-18.30.00', 1, 2, 1, 35.50, 'EUR', 35455);
-INSERT INTO precios (start_date, end_date, brand_id, price_list, priority, precio, curr, id_producto) VALUES ('2020-06-14-15.00.00', '2020-06-15-11.00.00', 1, 3, 1, 35.50, 'EUR', 35455);
-INSERT INTO precios (start_date, end_date, brand_id, price_list, priority, precio, curr, id_producto) VALUES ('2020-06-15-16.00.00', '2020-12-31-23.59.59', 1, 4, 1, 35.50, 'EUR', 35455);
+INSERT INTO precios (start_date, end_date, brand_id, price_list, priority, precio, curr, id_producto) VALUES ('2020-06-14T00.00.00', '2020-12-31T23.59.59', 1, 1, 0, 35.50, 'EUR', 35455);
+INSERT INTO precios (start_date, end_date, brand_id, price_list, priority, precio, curr, id_producto) VALUES ('2020-06-14T15.00.00', '2020-06-14T18.30.00', 1, 2, 1, 35.50, 'EUR', 35455);
+INSERT INTO precios (start_date, end_date, brand_id, price_list, priority, precio, curr, id_producto) VALUES ('2020-06-14T15.00.00', '2020-06-15T11.00.00', 1, 3, 1, 35.50, 'EUR', 35455);
+INSERT INTO precios (start_date, end_date, brand_id, price_list, priority, precio, curr, id_producto) VALUES ('2020-06-15T16.00.00', '2020-12-31T23.59.59', 1, 4, 1, 35.50, 'EUR', 35455);
