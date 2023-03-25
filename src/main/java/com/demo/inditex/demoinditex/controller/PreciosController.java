fdca3c37c1d66d09ac3cf7149f7190c3b1c5185f
m4st3 <runtycybin@gmail.com>
2023-03-25 18:15:28 +0100

    PreciosController update

--- src/main/java/com/demo/inditex/demoinditex/controller/PreciosController.java
+++ src/main/java/com/demo/inditex/demoinditex/controller/PreciosController.java
@@ -3,6 +3,7 @@ package com.demo.inditex.demoinditex.controller;
 import com.demo.inditex.demoinditex.dto.PriceRequestDTO;
 import com.demo.inditex.demoinditex.dto.PriceResponseDTO;
 import com.demo.inditex.demoinditex.service.PrecioService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,7 +24,7 @@ public class PreciosController {
 
     @GetMapping("/validatePrices")
     public ResponseEntity<List<PriceResponseDTO>> getPrices(
-            @RequestBody PriceRequestDTO priceRequestDTO) {
+            @RequestBody @Valid PriceRequestDTO priceRequestDTO) {
         return new ResponseEntity<>(precioService.validate(priceRequestDTO), HttpStatus.OK);
     }
 }
